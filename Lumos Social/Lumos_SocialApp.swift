f50855a45d77f1661898ea2b462972d1e52e74bd
armaan-crypto <armaanahmed08@gmail.com>
2023-12-04 12:21:33 -0500

    Initial Commit

---
+++ Lumos Social/Lumos_SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lumos_SocialApp.swift
+//  Lumos Social
+//
+//  Created by Armaan Ahmed on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Lumos_SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
